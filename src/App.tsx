ba6374c135adc9ebf2866d09ea8859ddcc0525e3
HYUNA <phyunaa0525@gmail.com>
2025-02-15 18:45:35 +0900

    add :: 의존성 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle, theme } from './styles/global'
 import './styles/glocal.css'
+import { Header } from './components/header'
+import { RouterProvider } from 'react-router-dom'
+import { Router } from './router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5000,
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
+      <QueryClientProvider client={queryClient} >
+        <GlobalStyle />
+        <Header />
+        <RouterProvider router={Router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }

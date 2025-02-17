2fc14242c34f398f830073d8ae17cf2495272229
HYUNA <phyunaa0525@gmail.com>
2025-02-17 18:22:19 +0900

    add :: 카드 컴포넌트

---
+++ src/components/Card.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components"
+import { Heart, HeartFill } from "../assets"
+
+interface CardProps {
+    title: string
+    hashtag: string[]
+    date: string
+    image: string
+    onClick: () => void
+}
+
+export const Card = ({ title, hashtag, date, image, onClick }: CardProps) => {
+    return (
+        <CardStyle onClick={onClick}>
+            <Image src={image} alt="card image" />
+            <InfoWrapper>
+                <div>
+                    <Title>{title}</Title>
+                    <HashTagList>
+                        {hashtag.map((tag, index) => <HashTag key={index}>#{tag}</HashTag>)}
+                    </HashTagList>
+                    <Date>{date}</Date>
+                </div>
+                <img src={Heart} alt="card image" width={38} height={38} />
+            </InfoWrapper>
+        </CardStyle>
+    )
+}
+
+const CardStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`
+const Title = styled.p`
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 6px;
+`
+const HashTagList = styled.div`
+    display: flex;
+    gap: 10px;
+    margin-bottom: 8px;
+`
+const HashTag = styled.p` 
+    font-size: 17px;
+    color: ${({ theme }) => theme.gray['01']};
+`
+const Image = styled.img`
+    width: 269px;
+    height: 285px;
+    object-fit: cover;
+    background-color: ${({ theme }) => theme.gray['02']};
+    border-radius: 6px;
+`
+
+const Date = styled.p`
+    font-size: 12px;
+    color: #9E9E9E;
+    margin-bottom: 10px;
+`
+
+
+const InfoWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`

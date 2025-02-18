24a803d8c76bfd568b98bfe874206527bfc26df2
HYUNA <phyunaa0525@gmail.com>
2025-02-18 22:34:00 +0900

    add :: 로그인 api연동

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,11 +5,14 @@ import { Back, SigninImg } from '../assets'
 import { Input } from '../components/input'
 import { Button } from '../components/button'
 import { Checkbox } from '../components/checkbox'
+import { UseLogin } from '../apis/users'
+import { cookie } from '../utils/auth'
 
 export const Login = () => {
   const [checked, setChecked] = useState<boolean>(false)
+  const { mutate: useLoginMutate } = UseLogin();
   const [loginData, setLoginData] = useState({
-    id: '',
+    accountId: '',
     password: '',
   })
 
@@ -38,10 +41,19 @@ export const Login = () => {
     setChecked(newChecked)
 
     if (newChecked) {
-      localStorage.setItem('id', loginData.id)
+      localStorage.setItem('id', loginData.accountId)
     } else {
       localStorage.removeItem('id')
     }
+  }
+
+  const Login = () => {
+    useLoginMutate(loginData, {
+      onSuccess: (data) => {
+        cookie.set('access_token', data.access_token)
+      }
+    }
+    )
   }
 
   return (
@@ -51,13 +63,13 @@ export const Login = () => {
       <LoginContentWrap>
         <InputWrap>
           <p>welcome back</p>
-          <Input onChange={handleChange} name="id" value={loginData.id} label="아이디" />
+          <Input onChange={handleChange} name="accountId" value={loginData.accountId} label="아이디" />
           <Input onChange={handleChange} name="password" value={loginData.password} label="비밀번호" type="password" />
           <IdSaveWrap>
             <Checkbox id="id-save" checked={checked} onChange={handleChangeCheckbox} />
             <IdSaveLabel htmlFor="id-save">아이디 저장</IdSaveLabel>
           </IdSaveWrap>
-          <Button onClick={() => {}}>로그인</Button>
+          <Button onClick={Login}>로그인</Button>
         </InputWrap>
         <LoginImgContent src={SigninImg} />
       </LoginContentWrap>
